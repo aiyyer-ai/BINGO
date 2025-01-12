b2d609ad8ea6dfd2a71073a13ac96e85443396be
Aiyyer <fifoescapes@gmail.com>
2025-01-12 10:50:11 -0800

    added master page

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let gridSize = {
 let possibleSquares = [
 	`"CUNT... Sorry"`,
 	`UNPROMPTED rags on gavin :)`,
-	`shane "predicts" what someone would say`,
+	`"predicts" what someone would say`,
 	`"you bitch!"`,
 	`tangent where shane fullscreen's camera`,
 	`shane makes a bad pun`,
@@ -43,15 +43,145 @@ let possibleSquares = [
 	`"deez nuts"`,
 	`whispers to self`,
 	`"i hate it here"`,
-
 ];
+
+var masterSquares = {};
+
+window.onload = (event) => {
+	//in case I want to make something run at launch
+	if(window.location.search.replace(`?`,``) == `master`) {
+		Array.from(document.querySelectorAll(`.notMaster`)).forEach((element) => {
+			element.style.display = `none`;
+		});
+		let centerDiv = document.getElementById(`center`);
+		let masterDiv = document.createElement(`div`);
+		masterDiv.classList.add(`masterBoard`);
+		masterDiv.id = `masterBoard`;
+		let allFactors = {};
+		let loopAttempts = 0;
+		while(Object.keys(allFactors).length == 0) {
+			allFactors = getPairedFactors(possibleSquares.length + loopAttempts);
+			for([key, pair] of Object.entries(allFactors)) {
+				if(pair[1] - pair[0] > 5) {
+					delete allFactors[key];
+				}
+			}
+			loopAttempts++;
+		}
+		let amountX = Object.values(allFactors)[0][1];
+		let amountY = Object.values(allFactors)[0][0];
+		let xSize = `calc(${100/amountX}% - ${gridSize.gap - gridSize.gap/amountX}px) `;
+		masterDiv.style.gridTemplateColumns = xSize.repeat(amountX);
+		let ySize = `calc(${100/amountY}% - ${gridSize.gap - gridSize.gap/amountY}px) `;
+		masterDiv.style.gridTemplateRows = ySize.repeat(amountY);
+		centerDiv.appendChild(masterDiv);
+		let titleTextBounds = document.getElementById(`title`).getBoundingClientRect();
+		masterDiv.style.top = titleTextBounds.bottom + 10 + "px";
+		for (text of possibleSquares) {
+		masterSquares[text] = false;
+		let newDiv = document.createElement(`div`);
+			newDiv.classList.add(`cell`);
+			newDiv.id = text;
+			let newH1 = document.createElement(`H1`);
+			newH1.classList.add(`bingoText`);
+			newH1.setAttribute(`data-dynamic-font-size`, "");
+			let newText = document.createTextNode(text.toUpperCase());
+			newH1.appendChild(newText);
+			newDiv.appendChild(newH1);
+			let bellaX = document.createElement(`div`);
+			bellaX.classList.add(`bellaX`);	
+			newDiv.appendChild(bellaX);
+			newDiv.bellaX = bellaX;
+			masterDiv.appendChild(newDiv);
+			newDiv.onclick = toggleMaster;
+		}
+		let checkBar = document.createElement(`div`);
+			checkBar.classList.add(`checkBar`);
+			centerDiv.appendChild(checkBar);
+			let checkInput = document.createElement(`input`);
+			checkInput.classList.add(`input`);
+			checkInput.setAttribute(`type`, "text");
+			checkInput.setAttribute(`id`, "checkInput");
+			checkInput.setAttribute(`maxLength`, "6");
+			checkInput.setAttribute(`placeholder`, "Card Seed");
+			checkBar.appendChild(checkInput);
+			let checkButton = document.createElement(`button`);
+			checkButton.classList.add(`button`);
+			checkButton.setAttribute(`onclick`, "checkBoardSeed()");
+			checkButton.innerHTML = `Check!`;
+			checkBar.appendChild(checkButton);
+			let masterBounds = masterDiv.getBoundingClientRect();
+			checkBar.style.top = masterBounds.top + "px";
+			checkBar.style.left = masterBounds.right + (window.innerWidth - masterBounds.right) / 2 - checkBar.clientWidth / 2 + "px";
+		let miniBingo = document.createElement(`div`);
+			miniBingo.classList.add(`miniBingo`);
+			centerDiv.appendChild(miniBingo);
+			miniBingo.style.width = window.innerWidth - masterBounds.right - 50 + "px";
+			miniBingo.style.height = window.innerWidth - masterBounds.right - 50 + "px";
+			miniBingo.style.top = checkBar.getBoundingClientRect().bottom + 10 + "px";
+			miniBingo.style.left = masterBounds.right + (window.innerWidth - masterBounds.right) / 2 - miniBingo.clientWidth / 2 + "px";
+			miniBingo.style.gap = gridSize.gap + "px";
+			let xSizeMini = `calc(${100/gridSize.x}% - ${gridSize.gap - 1}px) `;
+			miniBingo.style.gridTemplateColumns = xSizeMini.repeat(gridSize.x);
+			let ySizeMini = `calc(${100/gridSize.y}% - ${gridSize.gap - 1}px) `;
+			miniBingo.style.gridTemplateRows = ySizeMini.repeat(gridSize.y);
+		for (row = 0; row < gridSize.x; row++) {
+			for (column = 0; column < gridSize.y; column++) {
+				let newDiv = document.createElement(`div`);
+				newDiv.classList.add(`cell`);
+				newDiv.id = `${row},${column}`;
+				let newH1 = document.createElement(`H1`);
+				newH1.classList.add(`bingoText`);
+				newH1.setAttribute(`data-dynamic-font-size-smaller`, "");
+				if(newDiv.id == `2,2`) {
+					let newText = document.createTextNode(`LATE (FREE SPACE)`);
+					newH1.appendChild(newText);
+				} else {
+					let newText = document.createTextNode(``);
+					newH1.appendChild(newText);
+				}
+				newDiv.appendChild(newH1);
+				newDiv.textNode = newH1;
+				let bellaX = document.createElement(`div`);
+				bellaX.classList.add(`bellaX`);	
+				newDiv.appendChild(bellaX);
+				newDiv.bellaX = bellaX;
+				miniBingo.appendChild(newDiv);
+			}
+		}
+		adjustTextSize();
+	}
+}
+	
+function checkBoardSeed() {
+	const inputData = document.getElementById("checkInput");
+	let inputSeed = inputData.value.toUpperCase();
+	setSeed(inputSeed);
+	let squaresCopy = [...possibleSquares];
+	shuffle(squaresCopy);
+	for (row = 0; row < gridSize.x; row++) {
+		for (column = 0; column < gridSize.y; column++) {
+			let checkSquare = document.getElementById(`${row},${column}`);
+			if(checkSquare.id != `2,2`) {
+				checkSquare.textNode.childNodes[0].nodeValue = squaresCopy[(row * gridSize.x) + column].toUpperCase();
+				if(masterSquares[squaresCopy[(row * gridSize.x) + column]]) {
+					checkSquare.bellaX.style.backgroundColor = `deeppink`;
+				} else {
+					checkSquare.bellaX.style.backgroundColor = ``;
+				}
+			} else {
+				checkSquare.bellaX.style.backgroundColor = `deeppink`;
+			}
+		}
+	}
+}
 
 function generateBoard(button) {
 	const inputBar = document.getElementById("seedInput");
 	const shaneGif = document.getElementById("gif");
 	const gabPeek = document.getElementById("gabpeek");
 	const bellsPeek = document.getElementById("bellspeek");
-	let randomSeed = inputBar.value;
+	let randomSeed = inputBar.value.toUpperCase();
 	inputBar.style.display = `none`;
 	button.style.display = `none`;
 	shaneGif.style.display = `none`;
@@ -67,7 +197,7 @@ function generateBoard(button) {
 	let xSize = `calc(${100/gridSize.x}% - ${gridSize.gap - 1}px) `;
 	bingoCard.style.gridTemplateColumns = xSize.repeat(gridSize.x);
 	let ySize = `calc(${100/gridSize.y}% - ${gridSize.gap - 1}px) `;
-	bingoCard.style.gridTemplateRows = ySize.repeat(gridSize.x);
+	bingoCard.style.gridTemplateRows = ySize.repeat(gridSize.y);
 	for (row = 0; row < gridSize.x; row++) {
 	  for (column = 0; column < gridSize.y; column++) {
 		  let newDiv = document.createElement(`div`);
@@ -95,72 +225,8 @@ function generateBoard(button) {
 	  }
 	}
   
-	const dynamicFontSize = (textLength) => {
-	  /**
-	   * Edit these values to suit your needs
-	   */
-	  // Them minimum and maximum font sizes in pixels
-	  const minFontSize = 14;
-	  const maxFontSize = 35;
-  
-	  // The minimum and maximum line lengths in characters that should affect size
-	  const minLineLength = 12;
-	  const maxLineLength = 50;
-  
-	  // The minimum and maximum viewport widths in pixels where resizing should occur
-	  const minViewportWidth = 1;
-	  const maxViewportWidth = 1400;
-  
-	  /**
-	   * Danger, calculations below, tread carefully
-	   */
-	  // Relative max font size, gets bigger the fewer characters there are, down to minLineLength
-	  const relativeMaxFontSize =
-		textLength < minLineLength
-		  ? maxFontSize
-		  : textLength > maxLineLength
-		  ? minFontSize
-		  : maxFontSize -
-			((maxFontSize - minFontSize) * (textLength - minLineLength)) /
-			  (maxLineLength - minLineLength);
-  
-	  // Relative max viewport width, gets smaller the bigger the font size so it doesn't resize too soon
-	  const relativeMaxViewportWidth =
-		maxViewportWidth * (minFontSize / relativeMaxFontSize);
-  
-	  // Relative min viewport width, gets bigger the smaller the font size so it doesn't resize too late
-	  const relativeMinViewportWidth =
-		minViewportWidth * (maxFontSize / relativeMaxFontSize);
-  
-	  // Viewport width calculations
-	  const viewportWidth =
-		(100 * (maxFontSize - minFontSize)) /
-		(relativeMaxViewportWidth - relativeMinViewportWidth);
-  
-	  // Relative font size calculation
-	  const relativeFontSize =
-		(relativeMinViewportWidth * maxFontSize -
-		  relativeMaxViewportWidth * minFontSize) /
-		(relativeMinViewportWidth - relativeMaxViewportWidth);
-  
-	  // The magic clamp
-	  return `clamp(${minFontSize / 16}rem, ${viewportWidth}vw + ${
-		relativeFontSize / 16
-	  }rem, ${relativeMaxFontSize / 16}rem)`;
-	};
-  
-	const dynamicFontEls = document.querySelectorAll("[data-dynamic-font-size]");
-  
-	dynamicFontEls.forEach((dynamicFontEl) => {
-	  dynamicFontEl.style.fontSize = dynamicFontSize(
-		dynamicFontEl.textContent.replace(/(<([^>]+)>)/gi, "").trim().length
-	  );
-	});
-  
-}
+	adjustTextSize();
 
-window.onload = (event) => {
-//in case I want to make something run at launch
 }
 
 function shuffle(array) {
@@ -186,6 +252,15 @@ function toggleRed() {
 		this.bellaX.style.backgroundColor = ``;
 	}
 	
+}
+
+function toggleMaster() {
+	masterSquares[this.id] = !masterSquares[this.id];
+	if(this.bellaX.style.backgroundColor != `deeppink`) {
+		this.bellaX.style.backgroundColor = `deeppink`;
+	} else {
+		this.bellaX.style.backgroundColor = ``;
+	}
 }
 
 function cyrb128(str) {
@@ -222,4 +297,97 @@ function sfc32(a, b, c, d) {
 function setSeed(seedString) {
 	var seedHash = cyrb128(seedString);
 	rand = sfc32(seedHash[0], seedHash[1], seedHash[2], seedHash[3]);
+}
+
+function getPairedFactors(num) {
+	const factors = {};
+	let factorNum = 0;
+  
+	for (let i = 1; i <= Math.sqrt(num); i++) {
+	  if (num % i === 0) {
+		factors[factorNum] = [i, num / i];
+	  }
+	}
+  
+	return factors;
+}
+
+function adjustTextSize() {
+	const dynamicFontSize = (textLength, halfTextSize = false) => {
+		/**
+		 * Edit these values to suit your needs
+		 */
+		// Them minimum and maximum font sizes in pixels
+		const minFontSize = 14;
+		const maxFontSize = 35;
+
+		// The minimum and maximum line lengths in characters that should affect size
+		const minLineLength = 12;
+		const maxLineLength = 50;
+
+		// The minimum and maximum viewport widths in pixels where resizing should occur
+		const minViewportWidth = 1;
+		const maxViewportWidth = 4000;
+
+		/**
+		 * Danger, calculations below, tread carefully
+		 */
+		// Relative max font size, gets bigger the fewer characters there are, down to minLineLength
+		const relativeMaxFontSize =
+			textLength < minLineLength
+			? maxFontSize
+			: textLength > maxLineLength
+			? minFontSize
+			: maxFontSize -
+				((maxFontSize - minFontSize) * (textLength - minLineLength)) /
+				(maxLineLength - minLineLength);
+
+		// Relative max viewport width, gets smaller the bigger the font size so it doesn't resize too soon
+		const relativeMaxViewportWidth =
+			maxViewportWidth * (minFontSize / relativeMaxFontSize);
+
+		// Relative min viewport width, gets bigger the smaller the font size so it doesn't resize too late
+		const relativeMinViewportWidth =
+			minViewportWidth * (maxFontSize / relativeMaxFontSize);
+
+		// Viewport width calculations
+		const viewportWidth =
+			(100 * (maxFontSize - minFontSize)) /
+			(relativeMaxViewportWidth - relativeMinViewportWidth);
+
+		// Relative font size calculation
+		const relativeFontSize =
+			(relativeMinViewportWidth * maxFontSize -
+			relativeMaxViewportWidth * minFontSize) /
+			(relativeMinViewportWidth - relativeMaxViewportWidth);
+
+		// The magic clamp
+		if(halfTextSize) {
+			return `clamp(${minFontSize / 32}rem, ${viewportWidth}vw + ${
+				relativeFontSize / 32
+			}rem, ${relativeMaxFontSize / 32}rem)`;
+		} else {
+			return `clamp(${minFontSize / 16}rem, ${viewportWidth}vw + ${
+				relativeFontSize / 16
+			}rem, ${relativeMaxFontSize / 16}rem)`;
+		}
+
+	};
+
+		const dynamicFontEls = document.querySelectorAll("[data-dynamic-font-size]");
+
+		dynamicFontEls.forEach((dynamicFontEl) => {
+			dynamicFontEl.style.fontSize = dynamicFontSize(
+				dynamicFontEl.textContent.replace(/(<([^>]+)>)/gi, "").trim().length
+			);
+		});
+
+		const dynamicSmaller = document.querySelectorAll("[data-dynamic-font-size-smaller]");
+
+		dynamicSmaller.forEach((dynamicFontSmall) => {
+			dynamicFontSmall.style.fontSize = dynamicFontSize(
+				dynamicFontSmall.textContent.replace(/(<([^>]+)>)/gi, "").trim().length,
+				true
+			);
+		});
 }
